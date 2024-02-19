84fafa0ca5c2c4332a6fafdbed956e47d8c8d5e8
ejjang2030 <ejjang2030@gmail.com>
2024-02-20 02:25:39 +0900

    setup: vite 사용 설정2

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Test from './web/src/share/test';
-import App from './App';
+import App from './web/src/App.tsx';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Test);
+AppRegistry.registerComponent(appName, () => App);
