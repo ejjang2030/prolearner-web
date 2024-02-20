546cc57b8e8a3e1fe92aa97aa9d76dccf90bd66b
ejjang2030 <ejjang2030@gmail.com>
2024-02-20 23:18:51 +0900

    remove all

--- ios/Pods/Target Support Files/Pods-prolearner-prolearnerTests/Pods-prolearner-prolearnerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_prolearner_prolearnerTests : NSObject
-@end
-@implementation PodsDummy_Pods_prolearner_prolearnerTests
-@end
--- web/src/share/test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-const Test = ({title}) => {
-  return <Text>안녕하세요! { title}</Text>;
-};
-
-export default Test;
