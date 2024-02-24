8b9a0029916dbae12b6242900cd42b1e7b68279b
ejjang2030 <ejjang2030@gmail.com>
2024-02-24 21:26:31 +0900

    fix: TailwindCSS 가능하도록 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 
-import "./index.css";
 import App from "./App";
 const queryClient = new QueryClient({
   defaultOptions: {
